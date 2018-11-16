fb633e4f3989078f2ff739b56a1410ffde8eb689
priya <priya.kulal@gmail.com>
2018-11-16 13:37:54 +0530

    First Commit

---
+++ src/main/java/com/controller/NewServlet.java
@@ -0,0 +1,47 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+public class NewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String  str= request.getParameter("uname");
+		PrintWriter pw=response.getWriter();
+		//System.out.println("Welcome to maven:"+str);
+		pw.print("Welcome"+str);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="NewServlet">
+<input type="text" name="uname">
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
